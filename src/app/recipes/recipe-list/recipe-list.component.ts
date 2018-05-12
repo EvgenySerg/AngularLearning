5028a8cee2a84be1bbbf10a1cadcc5be0186b67c
fatboa <fatboa@yandex.ru>
2018-05-12 18:33:04 +0300

    Added recipies and recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+      new Recipe('Test Recipe', 'This is simply a test.', 'http://img.povar.ru/main/bb/f4/6e/19/sirniki_klassicheskii_recept-194902.JPG'),
+      new Recipe('Test Recipe', 'This is simply a test.', 'https://images.lady.mail.ru/5309/'),
+  ];
 
   constructor() { }
 
